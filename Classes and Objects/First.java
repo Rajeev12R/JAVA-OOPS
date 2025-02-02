2e24aa9990dfa957e691c3606bb14ad681bacb19
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-02-02 15:33:01 +0530

    Concept of classes, objects, multiple objects

--- Classes and Objects/First.java
+++ Classes and Objects/First.java
@@ -20,9 +20,27 @@ Object : An object is created from a class. We have already created the class na
 
 public class First{
     int a = 10;
+
+    int x = 40;
+    int y = 38;
     public static void main(String args[]){
         First firstObj = new First();  // firstObj is the Object of the class First. In JAVA Everything is done using classes.
         System.out.println(firstObj.a);
+        
+        // Lets try Multiple Objects -> 
+        
+        First secondObj = new First();
+        First thirdObj = new First();
+
+        System.out.println(secondObj.x );  // 40
+        System.out.println(thirdObj.y );    // 38
+
+        System.out.println(secondObj.y + thirdObj.y ); // 76
+        System.out.println(secondObj.x - thirdObj.y ); // 2
+
+
+
 
     }
 }
+
