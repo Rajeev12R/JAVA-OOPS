127305f42fcdad73a2dd3fc991c7dd564619fc78
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-02-02 15:39:39 +0530

    Use of Functions/ Method in the class

---
+++ Classes and Objects/Functions.java
@@ -0,0 +1,13 @@
+public class Functions {
+
+    // myFunc is a method/function 
+    static void myFunc(){
+        System.out.println("Hello I am a Function Called");
+    }
+
+    public static void main(String[] args) {
+        myFunc(); // Here function is called. 
+
+        // Output : Hello I am a Function Called
+    }
+}
