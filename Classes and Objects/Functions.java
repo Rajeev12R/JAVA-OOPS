b1b706ac333b50fec261d7ed3301db14756aa847
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-02-02 15:46:59 +0530

    STATIC VS PUBLIC Functions/ Method in the class

--- Classes and Objects/Functions.java
+++ Classes and Objects/Functions.java
@@ -5,9 +5,26 @@ public class Functions {
         System.out.println("Hello I am a Function Called");
     }
 
+    public void myPublicFunc(){
+        System.out.println("Hello I am a Public Function");
+    }
+
     public static void main(String[] args) {
         myFunc(); // Here function is called. 
 
-        // Output : Hello I am a Function Called
+        /* Output : Hello I am a Function Called
+
+        For public function creating a object named myObj
+        */
+        Functions myObj = new Functions();
+
+        myObj.myPublicFunc();   // This is how public function is called
     }
 }
+
+/* 
+Types of Methods (Static VS Public)
+
+We created a static method, which means that it can be accessed without creating an object of the class, unlike public, which can only be accessed by objects
+
+*/
