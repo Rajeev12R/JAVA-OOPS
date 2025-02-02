ddf7c0cc2281827e288cbe9510608a94df2a4dd1
Rajeev Ranjan <ranjan@Rajeevs-MacBook-Air-3.local>
2025-02-02 16:40:01 +0530

    Modifiers Overview

---
+++ Classes and Objects/Modifiers.java
@@ -0,0 +1,20 @@
+/* Modifiers are of two types 
+
+    Access Modifiers - It controls the access level
+    Non-Access Modifiers - It do not control access level, but provides other functionality
+
+    Access Modifier - 
+    public- The code is accessible for all classes
+    private- The code is only accessible within the declared class
+    default- The code is only accessible in the same package. This is used when you don't specify a modifier.
+    protected- The code is accessible in the same package and subclasses. 
+
+    Non-Access Modifier - 
+    final- The class cannot be inherited by other classes
+    abstract- The class cannot be used to create objects (To access an abstract class, it must be inherited from another class.)
+    
+*/
+
+public class Modifiers {
+    
+}
